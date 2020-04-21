3f57bae0ad17dd408a76facf7e50988832abe2cd
Gaál Gergely <gaalgergely@yahoo.com>
2020-04-21 20:44:00 +0200

    Application layout

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+/**
+ * @example manual logout
+ */
+Route::get('/logout-manual', function () {
+    request()->session()->invalidate();
+});
+
 Route::get('/{any}', 'AppController@index')->where('any', '.*');
